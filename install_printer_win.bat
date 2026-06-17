c946afa356a5138007b4b62d854c7e129a367cf3
perfectiv <166200970+perfectiv@users.noreply.github.com>
2026-06-17 18:22:47 +0700

    new printer ip

--- install_printer_win.bat
+++ install_printer_win.bat
@@ -2,14 +2,14 @@
 echo === Dang cai dat may in tren Windows ===
 
 :: Tao cong IP voi ten tuy bien
-cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r Ricoh_MP_5001_LAU6_13 -h 192.168.88.13 -o raw -n 9100
-cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r Ricoh_MP_2852_LAU3_12 -h 192.168.88.12 -o raw -n 9100
-cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r Ricoh_MP_3053_LAU7_232 -h 192.168.88.232 -o raw -n 9100
+cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r PER_PRINTER_L3 -h 192.168.88.13 -o raw -n 9100
+cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r PER_PRINTER_ACCOUNTING_L2 -h 192.168.88.12 -o raw -n 9100
+cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r PER_PRINTER_FA_L2 -h 192.168.88.14 -o raw -n 9100
 
 :: Cai may in voi ten + lau, tro vao cong tuong ung, dung driver co san
-rundll32 printui.dll,PrintUIEntry /if /b "Ricoh_MP_5001_LAU6" /r "Ricoh_MP_5001_LAU6_13" /m "Microsoft PCL6 Class Driver" /z /q
-rundll32 printui.dll,PrintUIEntry /if /b "Ricoh_MP_2852_LAU3" /r "Ricoh_MP_2852_LAU3_12" /m "Microsoft PCL6 Class Driver" /z /q
-rundll32 printui.dll,PrintUIEntry /if /b "Ricoh_MP_3053_LAU7" /r "Ricoh_MP_3053_LAU7_232" /m "Microsoft PCL6 Class Driver" /z /q
+rundll32 printui.dll,PrintUIEntry /if /b "PER_PRINTER_L3" /r "PER_PRINTER_L3" /m "Microsoft PCL6 Class Driver" /z /q
+rundll32 printui.dll,PrintUIEntry /if /b "PER_PRINTER_ACCOUNTING_L2" /r "PER_PRINTER_ACCOUNTING_L2" /m "Microsoft PCL6 Class Driver" /z /q
+rundll32 printui.dll,PrintUIEntry /if /b "PER_PRINTER_FA_L2" /r "PER_PRINTER_FA_L2" /m "Microsoft PCL6 Class Driver" /z /q
 
 echo === Hoan tat cai dat may in ===
 pause
